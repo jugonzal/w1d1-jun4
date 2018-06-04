86f9b945a9cc027a857714d4e19327c5b1774ff7
Juan Gonzalez <gonzalez@teach.cs.toronto.edu>
2018-06-04 16:17:54 -0400

    also implemented combinations

---
+++ pwdvalid.js
@@ -0,0 +1,39 @@
+// read a password from the command line
+// check if it is long
+// combination of numbers and letters
+// not '123456' or 'password'
+// try to add symbols
+// upper and lower case
+// minimum length
+
+// only needed for testing
+// var something0= process.argv[0];
+// var something1 = process.argv[1];
+
+var password = process.argv[2];
+
+// console.log(something0,something1, password);
+
+if (password.length > 6) {
+  console.log("OK. Password is long enough")
+}
+
+var yesNumbers = false;
+var yesLetters = false;
+var allNumbers = "0123456789";
+var allLetters = "qwertyuiopasdfghjklzxcvbnm"
+for (var i=0; i< password.length; i++) {
+  if (allNumbers.includes(password[i])) {
+    yesNumbers = true;
+  }
+  if (allLetters.includes(password[i])) {
+    yesLetters = true;
+  }
+}
+if (yesNumbers && yesLetters) {
+  console.log("OK. Password has good combination of letters and numbers");
+} else if (yesNumbers) {
+  console.log("NOT OK. Only numbers")
+} else if (yesLetters) {
+  console.log("NOT OK. Only letters")
+}
